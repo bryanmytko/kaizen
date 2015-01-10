b3e962366b37f796b649f8e8739b6785537b98c9
Bryan Mytko <bryanmytko@gmail.com>
2015-01-10 16:50:53 -0500

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141228070853) do
+ActiveRecord::Schema.define(version: 20150110214439) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "entries", force: :cascade do |t|
-    t.string   "type_of"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table "activities", force: :cascade do |t|
+    t.string   "title",       null: false
+    t.string   "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
+  create_table "entries", force: :cascade do |t|
+    t.text     "comment"
+    t.integer  "activity_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  add_index "entries", ["activity_id"], name: "index_entries_on_activity_id", using: :btree
+
+  add_foreign_key "entries", "activities"
 end
