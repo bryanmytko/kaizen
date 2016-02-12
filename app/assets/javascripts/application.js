05d0888ea06defa46a059cdc6a4297636628996c
Bryan Mytko <bryanmytko@gmail.com>
2016-02-12 14:36:07 -0500

    Add tooltips

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,20 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+//
+$(document).ready(function(){
+  Tipped.create(".unit", function(el){
+    var date = $(el).data().date;
+    var complete = $(el).data().complete;
+
+    if(complete === true){
+      var status = '<span class="ok">✓</span>';
+    }
+    else {
+      var status = '<span class="x">X</span>';
+    }
+
+    formatted_date = new Date(date);
+    return status + " " + formatted_date.toLocaleDateString();
+  });
+});
