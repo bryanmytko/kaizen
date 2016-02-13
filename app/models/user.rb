f3682bd75975e96745f22835ce5a3d8602a788af
Bryan Mytko <bryanmytko@gmail.com>
2016-02-13 03:41:59 -0500

    Add User

---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ActiveRecord::Base
+  has_many :activities
+
+  has_secure_password
+
+  validates :password, length: { minimum: 5 }
+  validates :email, presence: true
+  validates_format_of :email, with: /\A[^@]+@([^@\.]+\.)+[^@\.]+\z/
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest) == password
+  end
+end
