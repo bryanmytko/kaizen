6b1c23206c410d0266e5acd87f96dc9d5661ae7d
Bryan Mytko <bryanmytko@gmail.com>
2016-02-13 20:24:40 -0500

    Add validations for email & password

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,14 @@ class User < ActiveRecord::Base
   has_secure_password
 
   validates :password, length: { minimum: 5 }
-  validates :email, presence: true
+  validates :email, presence: true, uniqueness: true
   validates_format_of :email, with: /\A[^@]+@([^@\.]+\.)+[^@\.]+\z/
 
   def is_password?(password)
     BCrypt::Password.new(self.password_digest) == password
+  end
+
+  def self.password_match
+    self.password
   end
 end
