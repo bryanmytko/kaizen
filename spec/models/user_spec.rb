81682c2f1a4c825af179e496f6e5d8d96ff0ba2e
Bryan Mytko <bryanmytko@gmail.com>
2016-02-13 20:26:23 -0500

    Update User specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "has a valid factory" do
+    user = FactoryGirl.build(:user)
+    expect(user).to be_valid
+  end
+
+  it "is invalid with a short password" do
+    user = FactoryGirl.build(:user, password: "123")
+    expect(user).to be_invalid
+  end
+
+  it "is invalid without a password" do
+    user = FactoryGirl.build(:user, password: nil)
+    expect(user).to be_invalid
+  end
+
+  it "is invalid with a non-unique email" do
+    existing_user = FactoryGirl.create(:user)
+    new_user = FactoryGirl.create(:user, email: existing_user.email)
+
+    expect(new_user).to be_invalid
+  end
+
+  it "is invalid withouth a proper email" do
+    user = FactoryGirl.build(:user, email: "foo")
+    expect(user).to be_invalid
+  end
+
+  it "is invalid withouth an email" do
+    user = FactoryGirl.build(:user, email: nil)
+    expect(user).to be_invalid
+  end
 end
