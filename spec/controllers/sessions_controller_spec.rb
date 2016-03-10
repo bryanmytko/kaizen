834c33397c788087ac351fe760ed188838fdd995
Bryan Mytko <bryanmytko@gmail.com>
2016-03-10 02:29:22 -0500

    Add specs based on coverage report. Fix #7

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -15,4 +15,29 @@ describe SessionsController do
       expect(session).to be_empty
     end
   end
+
+  describe "POST login" do
+    context "when a user is valid" do
+      let(:password) { "password" }
+      let(:user){ FactoryGirl.create(:user, password: password) }
+
+      it "logs a user in" do
+        post :login, { session: { email: user.email, password: password } }
+        expect(response).to redirect_to(root_path)
+      end
+    end
+
+    context "when a user is not valid" do
+      it "it displays an error" do
+        post :login, {
+          session: {
+            email: "invalid@invalid.com",
+            password: "password123"
+          }
+        }
+        expect(flash).to_not be_empty
+        expect(response).to render_template(:index)
+      end
+    end
+  end
 end
