167dbac84c09ebc9428f123bf11e0105f5b185e7
Bryan Mytko <bryanmytko@gmail.com>
2016-03-10 01:43:43 -0500

    Add about_controller specs

--- spec/controllers/about_controller_spec.rb
+++ spec/controllers/about_controller_spec.rb
@@ -1,4 +1,10 @@
 require 'rails_helper'
 
-RSpec.describe AboutController, type: :controller do
+describe AboutController do
+  describe "#index" do
+    it "renders the about page" do
+      get :index
+      expect(response).to render_template :index
+    end
+  end
 end
