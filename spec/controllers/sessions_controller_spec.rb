d1364d5c54cb34ea54b6319c47c4faded31b1e4c
Bryan Mytko <bryanmytko@gmail.com>
2016-03-10 01:56:52 -0500

    Add session controller specs

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
-RSpec.describe SessionsController, type: :controller do
+describe SessionsController do
+  describe "GET #index" do
+    it "renders the login page (index)" do
+      get :index
+      expect(response).to render_template(:index)
+    end
+  end
 
+  describe "GET #logout" do
+    it "logs the user out" do
+      get :logout
+      expect(response).to redirect_to(login_path)
+      expect(session).to be_empty
+    end
+  end
 end
